582a99f42ce5fb38e445c2931fcd33687e2e7f67
Alistair Young <avatar@arkane-systems.net>
2022-11-08 17:00:31 -0600

    Working version.

---
+++ UpdateStargateIPv6Address/run.ps1
@@ -0,0 +1,67 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "UpdateStargateIPv6Address function processed a request."
+
+# Interact with query parameters or the body of the request.
+$name = $Request.Query.Name
+$zone = $Request.Query.Zone
+$reqIP = $Request.Query.reqIP
+
+if (-not $name) {
+    $name = $Request.Body.Name
+}
+
+if (-not $zone) {
+    $name = $Request.Body.Zone
+}
+
+if (-not $name) {
+    $name = $Request.Body.Name
+}
+
+If ($name -and $zone -and $reqIP) {
+    #Check if name passed is already in DNS zone that was passed
+    Try {$CurrentRec=Get-AzDnsRecordSet -Name $name -RecordType AAAA -ZoneName $zone -ResourceGroupName Standard}
+    Catch { write-host "Caught an exception:" -ForegroundColor Red
+            write-host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
+            write-host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red }
+
+            If ($CurrentRec) {
+                Write-Host "There is a current AAAAA record for $name in zone $zone"
+                
+                # Check current record IP against requested IP
+                $CurrIP=$CurrentRec.Records.Ipv6Address
+                if ($CurrIP -ne $reqIP) {
+                    Write-Host "IP Address $reqIP passed - updating DNS record accordingly"
+                    $CurrentRec.Records[0].Ipv6Address = $reqIP
+                    Set-AzDnsRecordSet -RecordSet $CurrentRec
+                    $body = "Updated DNS record with requested IP $reqIP"
+                    $status = [HttpStatusCode]::OK
+                    Write-Host $body
+                } else {
+                    $body = "Requested IP and current DNS record match - no changes needed"
+                    $status = [HttpStatusCode]::OK
+                    Write-Host $body
+                }
+            } else {
+                Write-Host "No current AAAA record for $name in zone $zone, adding now."
+                New-AzDnsRecordSet -Name $name -RecordType AAAA -ZoneName $zone -ResourceGroupName Standard -Ttl 3600 -DnsRecords (New-AzDnsRecordConfig -Ipv6Address $reqIP)
+                $status = [HttpStatusCode]::OK
+                $body = "DNS Record created with requested IP $reqIP"
+                Write-Host $body
+            }
+        } else {
+            $status = [HttpStatusCode]::BadRequest
+            $body = "Please pass a name, zone, and reqIP on the query string or in the request body."
+            Write-Host $body
+        }
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $status
+    Body = $body
+})
---
+++ profile.ps1
@@ -0,0 +1,10 @@
+# Azure Functions profile.ps1
+#
+# This profile.ps1 will get executed every "cold start" of your Function App.
+# "cold start" occurs when:
+#
+# * A Function App starts up for the very first time
+# * A Function App starts up after being de-allocated due to inactivity
+#
+
+Connect-AzAccount -Identity
